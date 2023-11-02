6bc59ca510b8d5aa511ecda7ac11b059a90771b6
Lovette <ASHLEY.LOVETTE@VIZIENTINC.COM>
2023-11-02 16:33:05 -0500

    install supabase, set up supabase service

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {SupabaseService} from '../services/supabase.service';
+import {SignInRequest} from '../models/sign-in-request';
 
 @Component({
   selector: 'app-sign-in',
@@ -8,13 +11,13 @@ import { Component } from '@angular/core';
               Welcome to the Habitat for Humanity Collin County Companion
           </div>
           <div>
-              <form>
+              <form [formGroup]="signInForm" (ngSubmit)="onSubmit()">
                   <div class="form-group">
-                      <input type="email" class="form-control text-box-input" id="exampleInputEmail1" aria-describedby="emailHelp"
+                      <input formControlName="email" type="email" class="form-control text-box-input" id="exampleInputEmail1" aria-describedby="emailHelp"
                              placeholder="email">
                   </div>
-                  <div class="form-group">
-                      <input type="password" class="form-control text-box-input" id="exampleInputPassword1" placeholder="password">
+                  <div class="form-group" >
+                      <input formControlName="password" type="password" class="form-control text-box-input" id="exampleInputPassword1" placeholder="password">
                   </div>
                 <div class="login-help">
                   <div class="form-check checkbox-container">
@@ -36,5 +39,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
+  loading = false;
 
+  signInForm = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+});
+
+  constructor(
+      private readonly supabase: SupabaseService
+  ) {}
+
+  async onSubmit() {
+    try {
+      this.loading = true
+      const request: SignInRequest = {
+        email: this.signInForm.value.email,
+        password: this.signInForm.value.password
+      }
+
+      const response = await this.supabase.signIn(request);
+
+      console.log(response);
+      alert('Check your email for the login link!')
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message)
+      }
+    } finally {
+      console.log()
+      this.signInForm.reset()
+      this.loading = false
+    }
+  }
 }
