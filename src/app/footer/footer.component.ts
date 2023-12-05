a71e8e31caa81d2890800a658aee3ca2a1a42698
Lovette <ASHLEY.LOVETTE@VIZIENTINC.COM>
2023-12-05 10:02:35 -0600

    no events styling, events tab footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   template: `<section class="footer">
-    <div class="tab">
+    <div class="tab" [ngClass]="{'selected': this.selectedTab == 'groups'}">
       <mat-icon>inbox</mat-icon>
       Groups
     </div>
-    <div class="tab selected">
+    <div class="tab" (click)="changeView('events')" [ngClass]="{'selected': this.selectedTab == 'events'}">
       <mat-icon>event_note</mat-icon>
       Events
     </div>
-    <div class="tab">
+    <div class="tab" [ngClass]="{'selected': this.selectedTab == 'profile'}">
       <mat-icon>account_circle</mat-icon>
       Profile
     </div>
@@ -19,5 +20,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  selectedTab: string = '';
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
+  changeView(selectedRoute: string) {
+    this.router.navigate([selectedRoute], {relativeTo: this.route});
 
+    this.selectedTab = selectedRoute;
+  }
 }
