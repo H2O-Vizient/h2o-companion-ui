dbcf3905228802cd6235acb7c123e4d51146c6fc
Lovette <ASHLEY.LOVETTE@VIZIENTINC.COM>
2023-12-05 12:24:27 -0600

    all-events styling, themes.scss variables

--- src/app/events/all-events/all-events.component.ts
+++ src/app/events/all-events/all-events.component.ts
@@ -7,39 +7,46 @@ import {FormControl, FormGroup} from '@angular/forms';
   template: `
       <div class="events">
           <form [formGroup]="findEventsForm">
-            <div class="select-group">
-              <div class="label">Choose a Date</div>
-              <select class="form-select" formControlName="date">
-                <option *ngFor="let date of dates" [value]="date">{{date}}</option>
-              </select>
-            </div>
+              <div class="select-group">
+                  <div class="label">Choose a Date</div>
+                  <div class="input-group">
+                      <span class="input-group-text"> <mat-icon class="calendar">calendar_today</mat-icon></span>
+                      <select class="form-select" formControlName="date">
+                          <option *ngFor="let date of dates" [value]="date">{{date}}</option>
+                      </select>
+                  </div>
+              </div>
 
 
-            <div class="select-group">
-              <div class="label">Choose a Location</div>
-              <select class="form-select" formControlName="date">
-                <option *ngFor="let location of locations" [value]="location">{{location}}</option>
-              </select>
-            </div>
+              <div class="select-group">
+                  <div class="label">Choose a Location</div>
+                  <div class="input-group">
+                      <span class="input-group-text"> <mat-icon class="location">location_on</mat-icon></span>
+                      <select class="form-select" formControlName="date">
+                          <option *ngFor="let location of locations" [value]="location">{{location}}</option>
+                      </select>
+                  </div>
+              </div>
           </form>
-        
-        <div class="event-list">
-          <div class="label">Choose an event</div>
-          
-          <div class="event-thumbnail" *ngFor="let event of events">
-            <mat-icon class="event-image"> insert_photo</mat-icon>
-            <div class="event-info">
-              <div class="name-date">
-                <div class="event-date">{{event.date}}</div>
-                <div class="event-name">{{event.eventName}}</div>
+
+          <div class="event-list">
+              <div class="label">Choose an event</div>
+
+              <div class="event-thumbnail" *ngFor="let event of events">
+                  <mat-icon class="event-image"> insert_photo</mat-icon>
+                  <div class="event-info">
+                      <div class="name-date">
+                          <div class="event-date">{{event.date}}</div>
+                          <div class="event-name">{{event.eventName}}</div>
+                      </div>
+                      <div class="event-location">
+                          <mat-icon class="event-location-icon">location_on</mat-icon>
+                          {{event.location}}
+                      </div>
+                      <hr>
+                  </div>
               </div>
-              <div class="event-location">
-                <mat-icon class="event-location-icon"> location_on</mat-icon>
-                {{event.location}}
-              </div>
-            </div>
           </div>
-        </div>
       </div>`,
   styleUrls: ['./all-events.component.scss']
 })
